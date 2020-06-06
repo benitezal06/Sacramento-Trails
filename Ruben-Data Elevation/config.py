95477f687133771bbd4a5ff327c2d51bb3d0f395
benitezal06 <57515868+benitezal06@users.noreply.github.com>
2020-06-05 21:30:15 -0700

    Add files via upload

---
+++ Ruben-Data Elevation/config.py
@@ -0,0 +1,2 @@
+# Enter your API key
+gkey = "AIzaSyChS2-JbkiWC6HXfxq2pgahjgQSGA3pY3w"
---
+++ config.py
@@ -0,0 +1,5 @@
+# REI Data API Key
+api_key = "200684868-72bf127ca9e9ba5924d3b7d37a05d12c"
+
+# Google API Key
+g_key = "AIzaSyArZ7QmzHIlMqoXbtU2Q7SrgrrrQMUitOM"
